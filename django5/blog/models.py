3b5f18613bda4db7cf9bfe096465e0c855e11d3a
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-03-29 15:37:50 +0000

    remove: unnecessary comment in models.py

--- django5/blog/models.py
+++ django5/blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-# Create your models here.
+
 class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
