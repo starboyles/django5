9dccc1b5d918a9f664d289adc7312771ffdb59da
Leslie Gyamfi <lesliegyamfi02@gmail.com>
2025-03-31 00:43:35 +0000

    refactor: update Post model fields and improve formatting

--- django5/blog/models.py
+++ django5/blog/models.py
@@ -1,31 +1,27 @@
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
-    PUBLISHED = 'PB', 'Published' 
+    PUBLISHED = 'PB', 'Published'
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DatetimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status, 
+        default=Status.DRAFT
+    )
 
-title = models.CharField(max_length=250)
-slug = models.SlugField(max_length=250)
-body = models.TextField()
-published = models.DateTimeField(default=timezone.now)
-created = models.DateTimeField(auto_now_add=True)
-updated = models.DateTimeField(auto_now_add=True)
-status = models.CharField(
-    max_length=2,
-    choices=Post.Status.choices,
-    default=Post.Status.DRAFT
-)
-
-
-class Meta: 
-    ordering = ['- publish']
-    indexes = [
-        models.Index(fields=['-publish'])
-    ]
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
 
     def __str__(self):
         return self.title
-    
